303ac97eb579c942a5ed315db32d9d0e3e07b342
sim.db_admin <sim.db_admin@firearrowsoftware.com>
2021-07-07 15:50:10 -0400

    Initial Check in
    Initial Check in

---
+++ src/PAModel/IR/IRNode.cs
@@ -0,0 +1,190 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.PowerPlatform.Formulas.Tools.Utility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.PowerPlatform.Formulas.Tools.IR
+{
+    internal abstract class IRNode
+    {
+        /// <summary>
+        /// Source Locations are only present when reading from source
+        /// And should not be expected during the unpack operation
+        /// </summary>
+        public SourceLocation? SourceSpan;
+        public abstract void Accept<Context>(IRNodeVisitor<Context> visitor, Context context);
+    }
+
+    /// <summary>
+    /// Represents block construct, may have 0-N child blocks and 0-N properties
+    /// </summary>
+    [DebuggerDisplay("{Name}: {Properties.Count} props")]
+    internal class BlockNode : IRNode, ICloneable<BlockNode>
+    {
+        public TypedNameNode Name;
+        public IList<PropertyNode> Properties = new List<PropertyNode>();
+        public IList<FunctionNode> Functions = new List<FunctionNode>();
+        public IList<BlockNode> Children = new List<BlockNode>();
+
+        public override void Accept<Context>(IRNodeVisitor<Context> visitor, Context context)
+        {
+            visitor.Visit(this, context);
+        }
+
+        public BlockNode Clone()
+        {
+            return new BlockNode()
+            {
+                Name = Name.Clone(),
+                Properties = Properties.Clone(),
+                Functions = Functions.Clone(),
+                Children = Children.Clone(),
+            };
+        }
+    }
+
+    /// <summary>
+    /// Represents names with types like
+    /// lblTest As label:
+    ///
+    /// where
+    /// Identifier = lblTest
+    /// Kind = label
+    /// </summary>
+    [DebuggerDisplay("{Identifier} as {Kind}")]
+    internal class TypedNameNode : IRNode, ICloneable<TypedNameNode>
+    {
+        public string Identifier;
+
+        /// <summary>
+        /// Kind is not required in all cases. 
+        /// </summary>
+        public TypeNode Kind;
+
+        public override void Accept<Context>(IRNodeVisitor<Context> visitor, Context context)
+        {
+            visitor.Visit(this, context);
+        }
+
+        public TypedNameNode Clone()
+        {
+            return new TypedNameNode()
+            {
+                Identifier = Identifier,
+                Kind = Kind.Clone()
+            };
+        }
+    }
+
+    /// <summary>
+    /// Represents a template like `label` or `gallery.HorizontalGallery`
+    /// </summary>
+    [DebuggerDisplay("{TypeName}.{OptionalVariant}")]
+    internal class TypeNode : IRNode, ICloneable<TypeNode>
+    {
+        public string TypeName;
+        public string OptionalVariant;
+
+        public override void Accept<Context>(IRNodeVisitor<Context> visitor, Context context)
+        {
+            visitor.Visit(this, context);
+        }
+
+        public TypeNode Clone()
+        {
+            return new TypeNode()
+            {
+                TypeName = TypeName,
+                OptionalVariant = OptionalVariant
+            };
+        }
+    }
+
+    [DebuggerDisplay("{Identifier}: ={Expression}")]
+    internal class PropertyNode : IRNode, ICloneable<PropertyNode>
+    {
+        public string Identifier;
+        public ExpressionNode Expression;
+
+        public override void Accept<Context>(IRNodeVisitor<Context> visitor, Context context)
+        {
+            visitor.Visit(this, context);
+        }
+
+        public PropertyNode Clone()
+        {
+            return new PropertyNode()
+            {
+                Identifier = Identifier,
+                Expression = Expression.Clone()
+            };
+        }
+    }
+
+    [DebuggerDisplay("{Identifier}({string.Join(',', Args)}):")]
+    internal class FunctionNode : IRNode, ICloneable<FunctionNode>
+    {
+        public string Identifier;
+        public IList<TypedNameNode> Args = new List<TypedNameNode>();
+        public IList<ArgMetadataBlockNode> Metadata = new List<ArgMetadataBlockNode>();
+
+        public override void Accept<Context>(IRNodeVisitor<Context> visitor, Context context)
+        {
+            visitor.Visit(this, context);
+        }
+
+        public FunctionNode Clone()
+        {
+            return new FunctionNode()
+            {
+                Identifier = Identifier,
+                Args = Args.Clone(),
+                Metadata = Metadata.Clone(),
+            };
+        }
+    }
+
+    [DebuggerDisplay("{Identifier}: ={Default}")]
+    internal class ArgMetadataBlockNode : IRNode, ICloneable<ArgMetadataBlockNode>
+    {
+        public string Identifier;
+        public ExpressionNode Default;
+
+        public override void Accept<Context>(IRNodeVisitor<Context> visitor, Context context)
+        {
+            visitor.Visit(this, context);
+        }
+
+        public ArgMetadataBlockNode Clone()
+        {
+            return new ArgMetadataBlockNode()
+            {
+                Identifier = Identifier,
+                Default = Default.Clone(),
+            };
+        }
+    }
+
+    [DebuggerDisplay("{Expression}")]
+    internal class ExpressionNode : IRNode, ICloneable<ExpressionNode>
+    {
+        public string Expression;
+        public override void Accept<Context>(IRNodeVisitor<Context> visitor, Context context)
+        {
+            visitor.Visit(this, context);
+        }
+
+        public ExpressionNode Clone()
+        {
+            return new ExpressionNode()
+            {
+                Expression = Expression
+            };
+        }
+    }
+}
---
+++ src/PAModel/MergeTool/Diff.cs
@@ -0,0 +1,103 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.PowerPlatform.Formulas.Tools.MergeTool.Deltas;
+using Microsoft.PowerPlatform.Formulas.Tools.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.PowerPlatform.Formulas.Tools.MergeTool
+{
+    internal static class Diff
+    {
+        public static IEnumerable<IDelta> ComputeDelta(CanvasDocument parent, CanvasDocument child)
+        {
+            var delta = new List<IDelta>();
+            foreach (var originalScreen in parent._screens)
+            {
+                if (child._screens.TryGetValue(originalScreen.Key, out var childScreen))
+                {
+                    delta.AddRange(ControlDiffVisitor.GetControlDelta(childScreen, originalScreen.Value, parent._editorStateStore));
+                }
+                else
+                {
+                    delta.Add(new RemoveControl() { ControlName = originalScreen.Key, ParentControlPath = ControlPath.Empty });
+                }
+            }
+            foreach (var newScreen in child._screens.Where(kvp => !parent._screens.ContainsKey(kvp.Key)))
+            {
+                delta.Add(new AddControl() { Control = newScreen.Value, ControlStates = child._editorStateStore.GetControlsWithTopParent(newScreen.Key).ToDictionary(state => state.Name), ParentControlPath = ControlPath.Empty });
+            }
+
+            var childTemplatesDict = child._templates.UsedTemplates.ToDictionary(temp => temp.Name.ToLower());
+            foreach (var template in child._templateStore.Contents)
+            {
+                if (parent._templateStore.TryGetTemplate(template.Key, out _))
+                    continue;
+
+                childTemplatesDict.TryGetValue(template.Key.ToLower(), out var jsonTemplate);
+
+                delta.Add(new AddTemplate() { Name = template.Key, Template = template.Value, JsonTemplate = jsonTemplate });
+            }
+
+            AddResourceDeltas(parent, child, delta);
+
+            AddDataSourceDeltas(parent, child, delta);
+
+            return delta;
+        }
+
+        private static void AddResourceDeltas(CanvasDocument parent, CanvasDocument child, List<IDelta> deltas)
+        {
+            var childResourcesDict = child._resourcesJson.Resources.ToDictionary(resource => resource.Name);
+            foreach (var resource in parent._resourcesJson.Resources)
+            {
+                if (childResourcesDict.ContainsKey(resource.Name))
+                {
+                    // $$$ Need resource contents diff here to detect resource replacements
+                    // eg, delete old resource, add new resource with same name but different contents
+
+                    childResourcesDict.Remove(resource.Name);
+                }
+
+                // We don't care about the asset files for remove for now,
+                // studio just won't load ones that don't have a ref in the json
+                // And they'll get cleaned up next save
+                // $$$ If we want a clean git history, we should apply that here
+                else
+                {
+                    deltas.Add(new RemoveResource() { Name = resource.Name });
+                }
+            }
+
+            foreach (var remaining in childResourcesDict)
+            {
+                FileEntry file = null;
+                if (remaining.Value.ResourceKind == Schemas.ResourceKind.LocalFile)
+                {
+                    var resourceName = remaining.Value.Path.Substring("Assets\\".Length);
+                    child._assetFiles.TryGetValue(FilePath.FromMsAppPath(resourceName), out file);
+                }
+                deltas.Add(new AddResource() { Name = remaining.Key, Resource = remaining.Value, File = file });
+            }
+        }
+
+        private static void AddDataSourceDeltas(CanvasDocument parent, CanvasDocument child, List<IDelta> deltas)
+        {
+            foreach (var ds in child._dataSources)
+            {
+                if (!parent._dataSources.ContainsKey(ds.Key))
+                    deltas.Add(new AddDataSource() { Name = ds.Key, Contents = ds.Value });
+            }
+
+
+            foreach (var ds in parent._dataSources)
+            {
+                if (!child._dataSources.ContainsKey(ds.Key))
+                    deltas.Add(new RemoveDataSource() { Name = ds.Key });
+            }
+        }
+    }
+}
